24dd6589e510a58ae01e185de4247118274e5c89
HarshilSutariya7777 <136455730+HarshilSutariya7777@users.noreply.github.com>
2024-05-06 15:39:42 +0530

    done

--- lib/Pages/VideoPlayer/Videoplayer.dart
+++ lib/Pages/VideoPlayer/Videoplayer.dart
@@ -1,5 +1,6 @@
 import 'package:cached_video_player/cached_video_player.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class VideoPlayerWidget extends StatefulWidget {
   final String videoPath;
@@ -47,11 +48,60 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
               setState(() {
                 isPlay = !isPlay;
               });
+              Get.to(FullScreenVideoPlayer(videoPlayerController));
             },
             icon: Icon(isPlay ? Icons.pause_circle : Icons.play_arrow),
           ),
         )
       ]),
+    );
+  }
+}
+
+class FullScreenVideoPlayer extends StatefulWidget {
+  final CachedVideoPlayerController videoPlayerController;
+
+  FullScreenVideoPlayer(this.videoPlayerController);
+
+  @override
+  State<FullScreenVideoPlayer> createState() => _FullScreenVideoPlayerState();
+}
+
+class _FullScreenVideoPlayerState extends State<FullScreenVideoPlayer> {
+  late bool isPlay;
+  @override
+  void initState() {
+    super.initState();
+    isPlay = !widget.videoPlayerController.value.isPlaying;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AspectRatio(
+          aspectRatio: widget.videoPlayerController.value.aspectRatio,
+          child: Stack(children: [
+            CachedVideoPlayer(widget.videoPlayerController),
+            Align(
+              alignment: Alignment.center,
+              child: IconButton(
+                onPressed: () {
+                  if (isPlay) {
+                    widget.videoPlayerController.play();
+                  } else {
+                    widget.videoPlayerController.pause();
+                  }
+                  setState(() {
+                    isPlay = !isPlay;
+                  });
+                },
+                icon: Icon(isPlay ? Icons.play_arrow : Icons.pause),
+              ),
+            ),
+          ]),
+        ),
+      ),
     );
   }
 }
